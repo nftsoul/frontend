5c1e3943edfc22d1cd93ce2d77541f9460155522
pashupati sah <pashupatisah112@gmail.com>
2022-01-29 18:40:40 +0545

    nfts name listing fixed

--- pages/premium-nfts.vue
+++ pages/premium-nfts.vue
@@ -33,7 +33,7 @@
                                         <v-card-subtitle class="text-left">{{item.gallery_name}}</v-card-subtitle>
                                         <v-row>
                                             <div class="prem-sup-card rounded-lg" v-for="(nft,i) in item.nfts" :key="i">
-                                                <span v-if="i<4">{{nft.title}}</span>
+                                                <span v-if="i<4">{{nft.name}}</span>
                                             </div>
                                         </v-row>
                                     </div>
