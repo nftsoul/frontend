583be27ed80d0bb69e69eaf6714362e26a35c039
Pashupati Sah <55347604+pashupatisah112@users.noreply.github.com>
2022-02-09 16:29:52 +0545

    zebec updated with new version in devnet

--- components/profile/preview.vue
+++ components/profile/preview.vue
@@ -124,7 +124,7 @@ export default {
     return {
       loading: false,
       connection: new web3.Connection(
-        web3.clusterApiUrl("mainnet-beta"),
+        web3.clusterApiUrl("devnet"),
         "confirmed"
       ),
     };
