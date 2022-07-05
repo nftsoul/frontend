b39938e0525d541dc310930ef0ab0371d1b9c6dd
Pashupati Sah <55347604+pashupatisah112@users.noreply.github.com>
2022-07-05 14:04:55 +0545

    metamask and nftsoul setup

--- components/dialog/wallet.vue
+++ components/dialog/wallet.vue
@@ -1,23 +1,23 @@
 <template>
 <div>
     <v-dialog v-model="walletDialog" max-width="400" persistent>
-        <div class="dark-bg">
-            <v-card color="background" outlined>
-                <p class="text-right">
-                    <v-icon @click="$store.commit('wallet/setWalletDialog',false)">mdi-close</v-icon>
-                </p>
-                <p class="text-center">Choose your wallet to continue</p>
-                <v-list-item dense v-for="(item,i) in wallets" :key="i">
-                    <v-list-item-avatar size="30">
-                        <v-img :src="require('~/assets/images/'+item.icon)"></v-img>
-                    </v-list-item-avatar>
-                    <v-list-item-content>
-                        <v-list-item-title>{{item.title}}</v-list-item-title>
-                    </v-list-item-content>
-                    <v-list-item-action>
-                        {{item.status}}
-                    </v-list-item-action>
-                </v-list-item>
+        <div class="white-border rounded-lg">
+            <v-card color="background" class="py-2 rounded-lg">
+                <v-col>
+                    <v-icon class="float-right" @click="$store.commit('wallet/setWalletDialog', false)">mdi-close</v-icon>
+                    <p class="text-center">Choose your wallet to continue</p>
+                    <v-list-item v-for="(item, i) in wallets" :key="i" @click="connect(item)">
+                        <v-list-item-avatar tile size="30">
+                            <v-img :src="require('~/assets/images/' + item.icon)"></v-img>
+                        </v-list-item-avatar>
+                        <v-list-item-content>
+                            <v-list-item-title>{{ item.title }}</v-list-item-title>
+                        </v-list-item-content>
+                        <v-list-item-action>
+                            <small>{{ item.status }}</small>
+                        </v-list-item-action>
+                    </v-list-item>
+                </v-col>
             </v-card>
         </div>
     </v-dialog>
@@ -29,27 +29,61 @@ export default {
     data() {
         return {
             wallets: [{
-                    icon: 'phantom.png',
-                    title: 'Phantom',
-                    status: null
+                    icon: "phantom.png",
+                    title: "Phantom",
+                    status: null,
+                    action: "phantom",
                 },
                 {
-                    icon: 'metamask.jpg',
-                    title: 'Metamask',
-                    status: null
-                }
-            ]
-        }
+                    icon: "metamask.jpg",
+                    title: "Metamask",
+                    status: null,
+                    action: "metamask",
+                },
+            ],
+        };
     },
     computed: {
         walletDialog: {
             get() {
-                return this.$store.state.wallet.walletDialog
+                return this.$store.state.wallet.walletDialog;
             },
             set(value) {
-                this.$store.commit('wallet/setWalletDialog', value)
+                this.$store.commit("wallet/setWalletDialog", value);
+            },
+        },
+    },
+    mounted() {
+        this.detectWallet();
+    },
+    methods: {
+        async detectWallet() {
+            var isPhantom = await this.$store.dispatch("wallet/detectPhantom");
+            var isMetamask = await this.$store.dispatch("wallet/detectMetamask");
+            if (isPhantom) {
+                this.wallets[0].status = "Detected";
             }
-        }
-    }
-}
+            if (isMetamask) {
+                this.wallets[1].status = "Detected";
+            }
+        },
+        connect(item) {
+            if (item.status != null) {
+                if (item.action == "phantom") {
+                    this.$store.dispatch("wallet/connectPhantom");
+                } else if (item.action == "metamask") {
+                    this.$store.dispatch("wallet/connectMetamask");
+                }
+            } else {
+                this.$toast
+                    .error('Please install wallet first.', {
+                        iconPack: "mdi",
+                        icon: "mdi-wallet",
+                        theme: "outline",
+                    })
+                    .goAway(3000);
+            }
+        },
+    },
+};
 </script>
