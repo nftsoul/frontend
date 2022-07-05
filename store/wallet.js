9f7e02fcbd9cddad19320384aabcc154b922fafc
Pashupati Sah <55347604+pashupatisah112@users.noreply.github.com>
2022-07-05 17:16:00 +0545

    metamask implementation first stage and once viewed always free view feature implememnted

--- store/wallet.js
+++ store/wallet.js
@@ -50,7 +50,10 @@ export const actions = {
   async checkLogin(context) {
     if (this.$auth.strategy.token.get()) {
       let profile = this.$auth.$storage.getUniversal("uni-nftsoul-user");
-      context.commit('setWallet',this.$auth.$storage.getUniversal("uni-nftsoul-wallet"))
+      context.commit(
+        "setWallet",
+        this.$auth.$storage.getUniversal("uni-nftsoul-wallet")
+      );
       context.commit("setWalletAddress", profile.user.wallet_address);
       context.commit("setProfile", profile.user);
       context.dispatch("getNotificationCount", profile.user);
@@ -166,12 +169,14 @@ export const actions = {
   },
   async connectMetamask(context) {
     try {
+      context.commit("setWalletDialog", false);
+
       const accounts = await ethereum.request({
         method: "eth_requestAccounts",
       });
       const account = accounts[0];
-      context.commit("setWalletAddress", account);
       context.commit("setWallet", "Metamask");
+      context.dispatch("signatureRequest", account);
     } catch (err) {
       this.$toast
         .error(err.message, {
@@ -183,9 +188,11 @@ export const actions = {
     }
   },
   async signatureRequest(context, walletAddress) {
+    let nonce = await context.dispatch("createNonce");
+    const message = `NFTsoul Authorization Sign In Request   Nonce:` + nonce;
+    let data = null;
+
     if (context.state.wallet == "Phantom") {
-      let nonce = await context.dispatch("createNonce");
-      const message = `NFTsoul Authorization Sign In Request   Nonce:` + nonce;
       const encodedMessage = new TextEncoder().encode(message);
       const signedMessage = await window.solana.signMessage(
         encodedMessage,
@@ -198,37 +205,60 @@ export const actions = {
       const signatureBytes = signedMessage.signature;
 
       var qs = require("qs");
-      var data = qs.stringify({
+      data = qs.stringify({
+        wallet: context.state.wallet,
         message: nacl.util.encodeBase64(messageBytes),
         wallet_address: walletAddress,
         publicKey: nacl.util.encodeBase64(publicKeyBytes),
         signature: nacl.util.encodeBase64(signatureBytes),
       });
-
-      try {
-        let response = await this.$auth.loginWith("local", {
-          data: data,
-        });
-        context.commit("setWalletAddress", walletAddress);
-        this.$auth.setUser(response.data.user);
-        this.$auth.$storage.setUniversal("uni-nftsoul-user", response.data)
-        this.$auth.$storage.setUniversal("uni-nftsoul-wallet", context.state.wallet)
-
-        context.commit("setProfile", response.data.user);
-        this.$axios.setToken(response.data.token, "X-XSRF-TOKEN");
-        context.dispatch("getNotificationCount", response.data.user);
-      } catch (e) {
-        this.$toast
-          .error("Login Failed", {
-            iconPack: "mdi",
-            icon: "mdi-wallet",
-            theme: "outline",
-          })
-          .goAway(3000);
-      }
     } else if (context.state.wallet == "Metamask") {
+      data = {
+        wallet: context.state.wallet,
+        wallet_address: walletAddress,
+      };
+      // console.log(window.ethereum.si)
+      // window.ethereum.personal.sign(
+      //   window.ethereum.fromUtf8(message),
+      //   walletAddress,
+      //   (err, signature) => {
+      //     console.log('err:',err,'signature:',signature)
+      //   }
+      // );
     } else {
-      console.log("alhuwa le:", context.wallet);
+      this.$toast
+        .error("Login failed", {
+          iconPack: "mdi",
+          icon: "mdi-wallet",
+          theme: "outline",
+        })
+        .goAway(3000);
+    }
+
+    try {
+      let response = await this.$auth.loginWith("local", {
+        data: data,
+      });
+      context.commit("setWalletAddress", walletAddress);
+      this.$auth.setUser(response.data.user);
+      this.$auth.$storage.setUniversal("uni-nftsoul-user", response.data);
+      this.$auth.$storage.setUniversal(
+        "uni-nftsoul-wallet",
+        context.state.wallet
+      );
+
+      context.commit("setProfile", response.data.user);
+      this.$axios.setToken(response.data.token, "X-XSRF-TOKEN");
+      context.dispatch("getNotificationCount", response.data.user);
+
+    } catch (e) {
+      this.$toast
+        .error("Login Failed", {
+          iconPack: "mdi",
+          icon: "mdi-wallet",
+          theme: "outline",
+        })
+        .goAway(3000);
     }
   },
   disconnect(context) {
@@ -237,12 +267,12 @@ export const actions = {
         method: "disconnect",
       });
     } else if (context.state.wallet == "Metamask") {
-      ethereum.on("disconnect");
+      // ethereum.on("disconnect");
     }
     context.commit("setWalletAddress", null);
     this.$auth.logout();
     this.$auth.$storage.removeUniversal("uni-nftsoul-user");
-    this.$auth.$storage.removeUniversal("uni-nftsoul-wallet")
+    this.$auth.$storage.removeUniversal("uni-nftsoul-wallet");
 
     this.$router.push("/");
   },
