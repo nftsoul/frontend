a7c94c7fe46d02b4e404f30e0faa65c0b7b1a512
Pashupati Sah <55347604+pashupatisah112@users.noreply.github.com>
2022-07-23 12:30:29 +0545

    display of a demo 3d showcase

--- components/section/mintShowcase/3dshow.vue
+++ components/section/mintShowcase/3dshow.vue
@@ -1,59 +1,69 @@
 <template>
-<div>
-    <div ref="canvas">
-
-    </div>
-</div>
+  <div>
+    <div ref="canvas"></div>
+  </div>
 </template>
 
 <script>
-let THREE = null
+let THREE = null;
 if (process.client) {
-    THREE = require('three');
+  THREE = require("three");
 }
 export default {
-    data() {
-        return {
-            scene: new THREE.Scene(),
-            camera: new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000),
-            renderer: new THREE.WebGLRenderer(),
-            boxGeometry: new THREE.BoxGeometry(2, 2, 2),
-            planeMaterial: new THREE.MeshBasicMaterial({
-                color: 0xFFFFFF
-            }),
-            boxMaterial: new THREE.MeshBasicMaterial({
-                // color: 0x000229
-            }),
-            box:null,
+  data() {
+    return {
+      scene: new THREE.Scene(),
+      camera: new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      ),
+      renderer: new THREE.WebGLRenderer(),
+    };
+  },
+  async mounted() {
+    //rendering the view section
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.$refs.canvas.appendChild(this.renderer.domElement);
+    // displaying axis and camera position
 
-        }
+    this.camera.position.set(-5, 2, 5);
+
+    //to move object freely using mouse and to zoom in and zoom out
+    let OC = require("three/examples/jsm/controls/OrbitControls.js");
+    const orbit = new OC.OrbitControls(this.camera, this.renderer.domElement);
+    orbit.update();
+
+    // 3d models
+    const modelLoader = require("three/examples/jsm/loaders/GLTFLoader.js");
+
+    // console.log(toy)
+    const assetLoader = await new modelLoader.GLTFLoader();
+    assetLoader.load(
+      "models/showcase.glb",
+      (gltf) => {
+        const model = gltf.scene;
+        this.scene.add(model);
+      },
+      undefined,
+      (error) => {
+        console.error(error);
+      }
+    );
+    // adding object to scene
+    this.renderer.setClearColor(0xc202d3);
+
+    const ambientLight = new THREE.AmbientLight(0xffffff);
+    this.scene.add(ambientLight);
+
+    //setting animation loop
+    this.renderer.setAnimationLoop(this.animate);
+  },
+  methods: {
+    animate() {
+      this.renderer.render(this.scene, this.camera);
     },
-    mounted() {
-        this.renderer.setSize(window.innerWidth, window.innerHeight)
-        this.$refs.canvas.appendChild(this.renderer.domElement)
-
-        const axesHelper = new THREE.AxesHelper(5)
-        this.camera.position.set(0, 2, 5);
-
-        //to move object freely using mouse and to zoom in and zoom out 
-        let OC = require("three/examples/jsm/controls/OrbitControls.js")
-        const orbit = new OC.OrbitControls(this.camera, this.renderer.domElement)
-        orbit.update()
-
-        this.box = new THREE.Mesh(this.boxGeometry, this.boxMaterial)
-
-        this.scene.add(axesHelper)
-        this.scene.add(this.box);
-
-        this.renderer.setAnimationLoop(this.animate)
-    },
-    methods: {
-        animate() {
-            this.box.rotation.x +=0.01
-            this.box.rotation.y += 0.01
-            this.renderer.render(this.scene, this.camera);
-        }
-    }
-
-}
+  },
+};
 </script>
